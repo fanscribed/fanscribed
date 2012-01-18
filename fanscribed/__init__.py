d0b80c694661f4c0407fac65ef1ca9785c1cc37f
Matthew Scott <matt@11craft.com>
2012-01-18 14:12:38 -0600

    Add skeletal 'edit' and 'view' views and templates.

--- fanscribed/__init__.py
+++ fanscribed/__init__.py
@@ -5,8 +5,10 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('fanscribed.views.my_view',
-                    context='fanscribed:resources.Root',
-                    renderer='fanscribed:templates/mytemplate.pt')
+    # Routes.
+    config.add_route('edit', '/edit/')
+    config.add_route('view', '/')
+    # Views.
+    config.scan('fanscribed')
     config.add_static_view('static', 'fanscribed:static', cache_max_age=3600)
     return config.make_wsgi_app()
--- fanscribed/views.py
+++ fanscribed/views.py
@@ -1,2 +1,23 @@
-def my_view(request):
-    return {'project':'fanscribed'}
+from pyramid.view import view_config
+
+
+@view_config(
+    route_name='edit',
+    context='fanscribed:resources.Root',
+    renderer='fanscribed:templates/edit.pt',
+)
+def edit(request):
+    return dict(
+        # ...
+    )
+
+
+@view_config(
+    route_name='view',
+    context='fanscribed:resources.Root',
+    renderer='fanscribed:templates/view.pt',
+)
+def view(request):
+    return dict(
+        # ...
+    )
