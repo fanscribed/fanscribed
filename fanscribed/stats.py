d6afb0d9396e3c9f4f4b291dcab569a25daac7f8
Matthew Scott <matt@11craft.com>
2012-01-31 22:01:02 -0600

    More stats: top transcribers and editors for each transcript - #40

--- fanscribed/stats.py
+++ fanscribed/stats.py
@@ -3,10 +3,13 @@
 import argparse
 from cgi import escape
 from collections import namedtuple
+from datetime import datetime
 from hashlib import sha1
-from operator import attrgetter
+from operator import attrgetter, itemgetter
 import os
 import sys
+from urllib2 import quote
+
 from ujson import load
 
 import git
@@ -50,6 +53,15 @@ class _SimpleObject(object):
             setattr(self, name, kwargs.get(name, None))
 
 
+class AuthorInfo(_SimpleObject):
+
+    __slots__ = [
+        'names',            # set()
+        'locks_created',    # dict(reponame={secret:Lock(), ...})
+        'snippets',         # dict(reponame={starting_point:[SnippetChange(), ...]})
+    ]
+
+
 class Lock(_SimpleObject):
 
     __slots__ = [
@@ -65,32 +77,29 @@ class Lock(_SimpleObject):
     ]
 
 
-class Snippet(_SimpleObject):
+class RepoInfo(_SimpleObject):
 
     __slots__ = [
-        'starting_point',   # ms
+        'name',             # str
+        'transcription',    # dict
+        'repo',             # Repo()
+        'authors',          # set()
+        'snippets',         # {starting_point: [SnippetChange(), ...]}
     ]
 
-    def __cmp__(self, other):
-        return cmp(self.starting_point, other.starting_point)
 
-    def __hash__(self):
-        return hash(self.starting_point)
+class SnippetChange(_SimpleObject):
+    """Represents the creation or modification of a snippet."""
+
+    __slots__ = [
+        'saved',            # time
+        'email',            # str
+    ]
 
 
 # Map author emails to stats.
 authors_map = {
-    # email: dict(
-    #   names=set(),
-    #   locks_created=dict(
-    #       reponame=dict(
-    #           secret=lock,
-    #       ),
-    #   ),
-    #   snippets=dict(
-    #       reponame={snippet, ...},
-    #   ),
-    # ),
+    # email: AuthorInfo(),
 }
 
 
@@ -138,7 +147,15 @@ def main():
             all_repos_valid = False
         else:
             repolog.info('repository OK')
-            repos[repo_path] = repo
+            master = repo.tree('master')
+            transcription_json = load(master['transcription.json'].data_stream)
+            repos[repo_path] = RepoInfo(
+                name=os.path.split(repo_path)[-1],
+                transcription=transcription_json,
+                repo=repo,
+                authors=set(),
+                snippets={},
+            )
     if not all_repos_valid:
         sys.exit(1)
     process_all_repos()
@@ -151,8 +168,9 @@ def main():
 
 def process_all_repos():
     """Loop through events in all repositories."""
-    for repo_path, repo in repos.iteritems():
-        repo_name = os.path.split(repo_path)[-1]
+    for repo_path, repo_info in repos.iteritems():
+        repo = repo_info.repo
+        repo_name = repo_info.name
         repolog = log.fields(repo_name=repo_name)
         repolog.info('processing')
         last_locks = {}
@@ -160,20 +178,20 @@ def process_all_repos():
             email = commit.author.email.lower()
             update_authors_map(email, commit)
             last_locks = update_locks(email, repo_name, commit, last_locks)
-            update_snippets(email, repo_name, commit)
+            update_snippets(email, repo_info, commit)
 
 
 def update_authors_map(email, commit):
     """Update the authors map based on the given commit."""
     author = commit.author
     if email not in authors_map:
-        authors_map[email] = dict(
+        authors_map[email] = AuthorInfo(
             names=set(),
             locks_created=dict(),
             snippets=dict(),
         )
     author_info = authors_map[email]
-    author_info['names'].add(author.name)
+    author_info.names.add(author.name)
 
 
 RotatedLock = namedtuple('RotatedLock', 'starting_point timestamp')
@@ -226,7 +244,7 @@ def update_locks(email, repo_name, commit, last_locks):
                     created_at=date,
                     created_by=email,
                 )
-                author_repo_locks_created = author_info['locks_created'].setdefault(repo_name, {})
+                author_repo_locks_created = author_info.locks_created.setdefault(repo_name, {})
                 author_repo_locks_created[secret] = new_lock
         # Pass along most recently-read locks to compare with next round.
         return locks
@@ -236,14 +254,23 @@ def update_locks(email, repo_name, commit, last_locks):
         return last_locks
 
 
-def update_snippets(email, repo_name, commit):
+def update_snippets(email, repo_info, commit):
+    repo_name = repo_info.name
+    repo_authors = repo_info.authors
+    repo_snippets = repo_info.snippets
     author_info = authors_map[email]
     for filename in commit.stats.files:
         if len(filename) == 20 and filename.endswith('.txt'):
+            # It's a snippet.  Find its starting point and create a SnippetChange instance.
             starting_point = int(filename[:16])
-            snippet = Snippet(starting_point=starting_point)
-            author_snippets = author_info['snippets'].setdefault(repo_name, set())
-            author_snippets.add(snippet)
+            snippet_change = SnippetChange(email=email, saved=commit.authored_date)
+            # Add the snippet change to the author's info.
+            author_repo_snippets = author_info.snippets.setdefault(repo_name, {})
+            author_repo_snippets_list = author_repo_snippets.setdefault(starting_point, [])
+            author_repo_snippets_list.append(snippet_change)
+            # Add the snippet change to the repo's info.
+            repo_snippets_list = repo_snippets.setdefault(starting_point, [])
+            repo_snippets_list.append(snippet_change)
 
 
 # ===================================================================
@@ -255,11 +282,20 @@ PAGE_TEMPLATE = """\
 <html>
     <head>
         <title>{title} - Stats - Fanscribed</title>
+        <style type="text/css">
+            #footer {{
+                font-style: italic;
+                border-top: thin solid black;
+            }}
+        </style>
     </head>
     <body>
         <h1>{title}</h1>
         <div>
             {body}
+        </div>
+        <div id="footer">
+            Generated at {timestamp}.
         </div>
     </body>
 </html>
@@ -269,33 +305,73 @@ PAGE_TEMPLATE = """\
 def ms_to_label(ms):
     seconds = ms / 1000
     minutes = seconds / 60
-    seconds = seconds % 60
+    seconds %= 60
     return '{0:d}m{0:02d}s'.format(minutes, seconds)
 
 
+def snippets_in_ms(ms, per_snippet=30000):
+    """Return the number of snippets possible in the given number of milliseconds."""
+    snippets_total = ms/ per_snippet
+    if ms % per_snippet:
+        # There is a partial snippet at the end.
+        snippets_total += 1
+    return snippets_total
+
+
 def author_names(info):
-    return u', '.join(sorted(info['names']))
+    return u', '.join(sorted(info.names))
 
 
 def author_filename(email):
     return 'author_{0}.html'.format(sha1(email).hexdigest())
 
 
+def transcript_filename(name):
+    return 'transcript_{0}.html'.format(name)
+
+
 def create_all_output(path):
     create_index(path)
+    create_all_transcript_pages(path)
     create_all_author_pages(path)
 
 
 def create_index(path):
     body = ''
+    # Transcripts.
+    # ------------
+    transcripts_list = [
+        # (name, html),
+    ]
+    for repo_path, repo_info in repos.iteritems():
+        name = repo_info.name
+        url = transcript_filename(name)
+        transcripts_list.append((
+            name,
+            '<li><a href="{url}">{name}</a></li>'.format(
+                url=quote(url),
+                name=name,
+            ),
+        ))
+    # Sort by name.
+    transcripts_list.sort(key=itemgetter(0))
+    body += """
+        <h2>Transcripts</h2>
+        <ul>
+            {transcripts_list}
+        </ul>
+    """.format(
+        transcripts_list='\n'.join(html for name, html in transcripts_list),
+    )
     # Authors.
+    # --------
     authors_list = [
         # (names, html),
     ]
     for email in sorted(authors_map):
-        info = authors_map[email]
+        author_info = authors_map[email]
         url = author_filename(email)
-        names = escape(author_names(info))
+        names = escape(author_names(author_info))
         authors_list.append((
             names,
             '<li><a href="{url}">{names}</a></li>'.format(
@@ -316,27 +392,111 @@ def create_index(path):
     write_page(
         path=path,
         filename='index.html',
-        title='Index',
+        title='Fanscribed Stats',
         body=body,
     )
 
 
+def create_all_transcript_pages(path):
+    for repo_path, repo_info in repos.iteritems():
+        name = repo_info.name
+        filename = transcript_filename(name)
+        body = ''
+        # General stats.
+        # ==============
+        body += '<h2>General stats</h2>'
+        total_snippets = snippets_in_ms(repo_info.transcription['duration'])
+        total_reviews = total_snippets - 1
+        repo = repo_info.repo
+        master = repo.tree('master')
+        remaining_reviews = len(load(master['remaining_reviews.json'].data_stream))
+        remaining_snippets = len(load(master['remaining_snippets.json'].data_stream))
+        percent_reviews = (remaining_reviews * 100) / total_reviews
+        percent_snippets = (remaining_snippets * 100) / total_snippets
+        snippets_completed = total_snippets - remaining_snippets
+        reviews_completed = total_reviews - remaining_reviews
+        body += """
+        <ul>
+            <li>Snippets transcribed: {snippets_completed} of {total_snippets} ({percent_snippets}%)</li>
+            <li>Reviews completed: {reviews_completed} of {total_reviews} ({percent_reviews}%)</li>
+        </ul>
+        """.format(**locals())
+        # Authors and editors
+        # ========
+        # Figure out who the top transcriptionists and editors are.
+        snippet_creators = {
+            # email: [starting_point, ...],
+        }
+        snippet_editors = {
+            # email: [starting_point, ...],
+        }
+        for starting_point, snippet_changes in repo_info.snippets.iteritems():
+            transcriptionist = snippet_changes[0].email
+            snippet_creator_snippets = snippet_creators.setdefault(transcriptionist, [])
+            snippet_creator_snippets.append(starting_point)
+            for snippet_change in snippet_changes[1:]:
+                editor = snippet_change.email
+                snippet_editor_snippets = snippet_editors.setdefault(editor, [])
+                snippet_editor_snippets.append(starting_point)
+        # Reverse sort by number of snippets created or edited.
+        snippet_creators = reversed(sorted(snippet_creators.iteritems(), key=lambda kv: len(kv[1])))
+        snippet_editors = reversed(sorted(snippet_editors.iteritems(), key=lambda kv: len(kv[1])))
+        snippet_creators = [
+            '<li><a href="{url}">{names}</a> ({count} snippets transcribed)</li>'.format(
+                url=author_filename(email),
+                names=author_names(authors_map[email]),
+                count=len(starting_points),
+            )
+            for email, starting_points
+            in snippet_creators
+        ]
+        snippet_editors = [
+            '<li><a href="{url}">{names}</a> ({count} snippets edited)</li>'.format(
+                url=author_filename(email),
+                names=author_names(authors_map[email]),
+                count=len(starting_points),
+            )
+            for email, starting_points
+            in snippet_editors
+        ]
+        body += """
+            <h2>Top transcriptionists</h2>
+            <ul>
+                {snippet_creators}
+            </ul>
+            <h2>Top reviewers/editors</h2>
+            <ul>
+                {snippet_editors}
+            </ul>
+        """.format(
+            snippet_creators='\n'.join(snippet_creators),
+            snippet_editors='\n'.join(snippet_editors),
+        )
+        write_page(
+            path=path,
+            filename=filename,
+            title='Transcript: {name}'.format(name=name),
+            body=body,
+        )
+
+
 def create_all_author_pages(path):
-    for email, info in authors_map.iteritems():
-        names = escape(author_names(info))
+    for email, author_info in authors_map.iteritems():
+        names = escape(author_names(author_info))
         body = ''
         # Snippets contributed to.
-        body += '<h2>Snippets contributed to</h2>'
-        snippets_map = authors_map[email]['snippets']
+        body += '<h2>Snippets transcribed or edited</h2>'
+        snippets_map = authors_map[email].snippets
         for repo_name in sorted(snippets_map):
-            snippets_list = sorted(snippets_map[repo_name])
+            snippet_change_map = snippets_map[repo_name]
+            starting_points = sorted(snippet_change_map)
             list_items = [
                 '<li><a href="http://{repo_name}/#{label}">{repo_name}/#{label}</a></li>'.format(
                     repo_name=repo_name,
-                    label=ms_to_label(snippet.starting_point)
+                    label=ms_to_label(starting_point)
                 )
-                for snippet
-                in snippets_list
+                for starting_point
+                in starting_points
             ]
             body += """
                 <h3>{repo_name}</h3>
@@ -356,6 +516,7 @@ def create_all_author_pages(path):
 
 
 def write_page(path, filename, **kwargs):
+    kwargs['timestamp'] = unicode(datetime.now())
     filename = os.path.join(path, filename)
     log.fields(filename=filename).info('writing')
     with open(filename, 'wb') as f:
