f8c95e2b35e29942a82ef2d8f29136f4166168a0
Matthew Scott <matt@11craft.com>
2012-02-12 19:37:19 -0600

    Clean up content cache along with audio snippet cache. - #42

--- fanscribed/cleanup.py
+++ fanscribed/cleanup.py
@@ -5,7 +5,8 @@ from paste.deploy.loadwsgi import loadapp
 from paste.script.command import Command
 
 
-THRESHOLD_SECONDS = 4 * 60 * 60 # 4 hours
+CONTENT_CACHE_THRESHOLD_SECONDS = 48 * 60 * 60 # 48 hours
+SNIPPET_CACHE_THRESHOLD_SECONDS = 4 * 60 * 60 # 4 hours
 
 
 class CleanupCommand(Command):
@@ -29,14 +30,32 @@ class CleanupCommand(Command):
         app = loadapp(app_spec, name='main', relative_to=base, global_conf={})
         # Read settings.
         settings = app.registry.settings
+        cache = settings['fanscribed.cache']
         snippet_cache = settings['fanscribed.snippet_cache']
         # Make sure paths exist.
+        if not os.path.isdir(cache):
+            print 'Cache path {0} does not exist'.format(cache)
+            return 1
         if not os.path.isdir(snippet_cache):
             print 'Snippet cache path {0} does not exist'.format(snippet_cache)
             return 1
+        # Go through all of the files in the content cache and delete ones
+        # not accessed within the threshold.
+        print 'Content cache',
+        oldest_allowed = time.time() - CONTENT_CACHE_THRESHOLD_SECONDS
+        unlinked_count = 0
+        filenames = os.listdir(cache)
+        for filename in filenames:
+            full_path = os.path.join(cache, filename)
+            atime = os.stat(full_path).st_atime
+            if atime < oldest_allowed:
+                os.unlink(full_path)
+                unlinked_count += 1
+        print 'Unlinked: {0}, Remaining: {1}'.format(unlinked_count, len(filenames) - unlinked_count)
         # Go through all of the files in the snippet cache and delete ones
         # not accessed within the threshold.
-        oldest_allowed = time.time() - THRESHOLD_SECONDS
+        print 'Snippet cache',
+        oldest_allowed = time.time() - SNIPPET_CACHE_THRESHOLD_SECONDS
         unlinked_count = 0
         filenames = os.listdir(snippet_cache)
         for filename in filenames:
