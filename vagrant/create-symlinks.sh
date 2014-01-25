b01fc9249eb8eb1f2e31349654ff98a3361cfc89
Matthew Scott <matt@11craft.com>
2014-01-25 15:26:02 -0600

    Update vagrant shared directories and provisioning steps

--- vagrant/create-symlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p /srv
-if [ ! -f /srv/pillar ]; then
-  ln -s /vagrant/fanscribed/salt/roots/pillar /srv/pillar
-fi
-if [ ! -f /srv/salt ]; then
-  ln -s /vagrant/fanscribed/salt/roots/salt /srv/salt
-fi
--- vagrant/install-kernel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# install the backported kernel
-
-apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
