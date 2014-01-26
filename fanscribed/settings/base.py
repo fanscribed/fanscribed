bd9da9f8ccf6ccecf0f9e9b500e584b863b0bee6
Matthew Scott <matt@11craft.com>
2014-01-26 17:53:21 -0600

    Also serialize in JSON so that it matches the accepted content type

--- fanscribed/settings/base.py
+++ fanscribed/settings/base.py
@@ -266,6 +266,7 @@ THIRD_PARTY_APPS += (
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = ['json']
 
 
 # APPS
