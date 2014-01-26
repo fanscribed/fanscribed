08be40356539ea69dd2c1f60c930c941c9f39f3a
Matthew Scott <matt@11craft.com>
2014-01-26 02:12:26 -0600

    Consolidate urls into one list

--- fanscribed/urls.py
+++ fanscribed/urls.py
@@ -8,14 +8,7 @@ admin.autodiscover()
 
 
 # URL patterns used by all projects based on fanscribed core.
-base_urlpatterns = patterns('',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-)
-
-# URL patterns used by only this project.
-core_urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
 )
-
-
-urlpatterns = base_urlpatterns + core_urlpatterns
