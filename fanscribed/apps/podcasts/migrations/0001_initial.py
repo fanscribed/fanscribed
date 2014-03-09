8e9f36dcf0f74033b4c68f0d30cb3b34a781cfc0
Matthew Scott <matt@11craft.com>
2014-03-09 16:22:26 -0500

    Add podcasts.TranscriptionApproval model class; add state machine docs

--- fanscribed/apps/podcasts/migrations/0001_initial.py
+++ fanscribed/apps/podcasts/migrations/0001_initial.py
@@ -11,27 +11,30 @@ class Migration(SchemaMigration):
         # Adding model 'Podcast'
         db.create_table(u'podcasts_podcast', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True)),
-            ('rss_url', self.gf('django.db.models.fields.URLField')(max_length=512)),
+            ('rss_url', self.gf('django.db.models.fields.URLField')(unique=True, max_length=512)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True)),
+            ('approval_state', self.gf('django_fsm.db.fields.fsmfield.FSMField')(default='not_approved', max_length=50)),
         ))
         db.send_create_signal(u'podcasts', ['Podcast'])
 
-        # Adding model 'Episode'
-        db.create_table(u'podcasts_episode', (
+        # Adding model 'TranscriptionApproval'
+        db.create_table(u'podcasts_transcriptionapproval', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('podcast', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['podcasts.Podcast'])),
-            ('title', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('approval_type', self.gf('django.db.models.fields.CharField')(max_length=5)),
+            ('notes', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
         ))
-        db.send_create_signal(u'podcasts', ['Episode'])
+        db.send_create_signal(u'podcasts', ['TranscriptionApproval'])
 
         # Adding model 'RssFetch'
         db.create_table(u'podcasts_rssfetch', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('created', self.gf('django.db.models.fields.DateField')()),
             ('podcast', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['podcasts.Podcast'])),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
             ('fetched', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
             ('body', self.gf('django.db.models.fields.BinaryField')(null=True, blank=True)),
-            ('state', self.gf('django_fsm.db.fields.fsmfield.FSMField')(default='not-fetched', max_length=50)),
+            ('state', self.gf('django_fsm.db.fields.fsmfield.FSMField')(default='not_fetched', max_length=50)),
         ))
         db.send_create_signal(u'podcasts', ['RssFetch'])
 
@@ -40,34 +43,73 @@ class Migration(SchemaMigration):
         # Deleting model 'Podcast'
         db.delete_table(u'podcasts_podcast')
 
-        # Deleting model 'Episode'
-        db.delete_table(u'podcasts_episode')
+        # Deleting model 'TranscriptionApproval'
+        db.delete_table(u'podcasts_transcriptionapproval')
 
         # Deleting model 'RssFetch'
         db.delete_table(u'podcasts_rssfetch')
 
 
     models = {
-        u'podcasts.episode': {
-            'Meta': {'object_name': 'Episode'},
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'podcast': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['podcasts.Podcast']"}),
-            'title': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         u'podcasts.podcast': {
             'Meta': {'object_name': 'Podcast'},
+            'approval_state': ('django_fsm.db.fields.fsmfield.FSMField', [], {'default': "'not_approved'", 'max_length': '50'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'rss_url': ('django.db.models.fields.URLField', [], {'max_length': '512'})
+            'rss_url': ('django.db.models.fields.URLField', [], {'unique': 'True', 'max_length': '512'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
         },
         u'podcasts.rssfetch': {
             'Meta': {'object_name': 'RssFetch'},
             'body': ('django.db.models.fields.BinaryField', [], {'null': 'True', 'blank': 'True'}),
-            'created': ('django.db.models.fields.DateField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
             'fetched': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'podcast': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['podcasts.Podcast']"}),
-            'state': ('django_fsm.db.fields.fsmfield.FSMField', [], {'default': "'not-fetched'", 'max_length': '50'})
+            'state': ('django_fsm.db.fields.fsmfield.FSMField', [], {'default': "'not_fetched'", 'max_length': '50'})
+        },
+        u'podcasts.transcriptionapproval': {
+            'Meta': {'object_name': 'TranscriptionApproval'},
+            'approval_type': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'podcast': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['podcasts.Podcast']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
         }
     }
 
--- fanscribed/apps/podcasts/models.py
+++ fanscribed/apps/podcasts/models.py
@@ -1,33 +1,127 @@
 import datetime
 
 from django.db import models
+from django.dispatch import receiver
 
 from django_extensions.db.models import CreationDateTimeField
 
+from django_fsm.signals import post_transition
 from django_fsm.db.fields import FSMField, transition
+
+import feedparser
 
 
 class Podcast(models.Model):
+    """A podcast we want to have in our directory.
 
-    name = models.CharField(max_length=200, blank=True, null=True)
-    rss_url = models.URLField(max_length=512)
+    approval_state
+    --------------
+
+    @startuml
+    [*] --> not_approved
+    not_approved --> owner_verified
+    not_approved --> user_approved
+    not_approved --> staff_approved
+    user_approved --> staff_approved
+    user_approved --> owner_verified
+    staff_approved --> owner_verified
+    @enduml
+    """
+
+    rss_url = models.URLField(max_length=512, unique=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    approval_state = FSMField(default='not_approved', protected=True)
+
+    # approval_state
+    # --------------
+
+    @transition(approval_state,
+                source='not_approved',
+                target='user_approved',
+                save=True)
+    def user_approve(self, user, notes=None):
+        TranscriptionApproval.objects.create(
+            podcast=self,
+            user=user,
+            approval_type='user',
+            notes=notes,
+        )
+
+    @transition(approval_state,
+                source=['not_approved', 'user_approved'],
+                target='staff_approved',
+                save=True)
+    def staff_approve(self, user, notes=None):
+        TranscriptionApproval.objects.create(
+            podcast=self,
+            user=user,
+            approval_type='staff',
+            notes=notes,
+        )
+
+    @transition(approval_state,
+                source=['not_approved', 'user_approved', 'staff_approved'],
+                target='owner_verified',
+                save=True)
+    def owner_verify(self, user, notes=None):
+        TranscriptionApproval.objects.create(
+            podcast=self,
+            user=user,
+            approval_type='owner',
+            notes=notes,
+        )
 
 
-class Episode(models.Model):
+APPROVAL_TYPE_CHOICES = [
+    ('user', 'User'),
+    ('staff', 'Staff'),
+    ('owner', 'Onwer'),
+]
 
-    podcast = models.ForeignKey(Podcast)
-    title = models.URLField(max_length=200, blank=True, null=True)
+class TranscriptionApproval(models.Model):
+    """
+    A record of a user indicating we are allowed to transcribe the podcast.
+    """
+
+    podcast = models.ForeignKey('Podcast')
+    user = models.ForeignKey('auth.User')
+    approval_type = models.CharField(max_length=5, choices=APPROVAL_TYPE_CHOICES)
+    notes = models.TextField(blank=True, null=True)
 
 
 class RssFetch(models.Model):
+    """A fetch of the RSS feed of a podcast.
+
+    @startuml
+    [*] --> not_fetched
+    not_fetched --> fetching
+    fetching --> fetched
+    fetching --> failed
+    not_fetched --> fetched
+    fetched --> [*]
+    failed --> [*]
+    @enduml
+    """
 
     podcast = models.ForeignKey(Podcast)
     created = CreationDateTimeField()
     fetched = models.DateField(blank=True, null=True)
     body = models.BinaryField(blank=True, null=True)
-    state = FSMField(default='not-fetched', protected=True)
+    state = FSMField(default='not_fetched', protected=True)
 
-    @transition(state, 'not-fetched', 'fetched')
+    @transition(state, 'not_fetched', 'fetched')
     def load_rss(self, body):
         self.body = body
         self.fetched = datetime.datetime.now()
+
+    @transition(state, 'not_fethed', 'fetching')
+    def start(self):
+        self.fetched = datetime.datetime.now()
+
+    @transition(state, 'fetching', 'fetched')
+    def finish(self, body):
+        self.body = body
+
+    @transition(state, 'fetching', 'failed')
+    def fail(self):
+        pass
