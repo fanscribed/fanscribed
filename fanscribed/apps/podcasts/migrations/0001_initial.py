eea558b9122b7d4988a02b2847d57db1d5153ab0
Matthew Scott <matt@11craft.com>
2014-04-10 16:46:32 -0500

    podcasts needs transcripts to exist first

--- fanscribed/apps/podcasts/migrations/0001_initial.py
+++ fanscribed/apps/podcasts/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = [
+        ('transcripts', '0001_initial'),
+    ]
+
     def forwards(self, orm):
         # Adding model 'Episode'
         db.create_table(u'podcasts_episode', (
