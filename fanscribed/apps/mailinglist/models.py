ed2452608cb343086d95caec4d1015d6d5edd52e
Matthew Scott <matt@11craft.com>
2014-04-15 08:40:59 -0500

    Move `receivers` module content into `models`

--- fanscribed/apps/mailinglist/models.py
+++ fanscribed/apps/mailinglist/models.py
@@ -1,1 +1,11 @@
-from . import receivers
+from django.dispatch import receiver
+
+from allauth.account.signals import user_signed_up
+
+from .tasks import add_user_email_to_mailchimp
+
+
+@receiver(user_signed_up)
+def add_to_mailchimp(sender, request, user, **kwargs):
+    for email in user.emailaddress_set.filter(primary=True):
+        add_user_email_to_mailchimp.delay(email.email, user.id)
