e06c62df5bd44111b5799480adab2b870d3547e0
Matthew Scott <matt@11craft.com>
2014-11-29 21:46:26 -0600

    Use correct database and redis locations when running backend tests

--- backend/fanscribed/settings/test.py
+++ backend/fanscribed/settings/test.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 from dj_database_url import parse
@@ -5,7 +7,12 @@ from dj_database_url import parse
 
 TESTING = True
 
-DATABASE_URL = getenv('DATABASE_URL', 'postgres://fanscribed:fanscribed@localhost:5432/fanscribed')
+DEFAULT_DATABASE_URL = (
+    'postgres://postgres@'
+    '{DB_PORT_5432_TCP_ADDR}:{DB_PORT_5432_TCP_PORT}/postgres'
+    .format(**os.environ)
+)
+DATABASE_URL = getenv('DATABASE_URL', DEFAULT_DATABASE_URL)
 DATABASES = {
     'default': parse(DATABASE_URL),
 }
@@ -21,10 +28,14 @@ TRANSCRIPT_FRAGMENT_OVERLAP = Decimal('1.50')
 # For most tests, don't worry about teamwork requirement when assigning tasks.
 TRANSCRIPTS_REQUIRE_TEAMWORK = False
 
+DEFAULT_TEST_REDIS_CACHE_LOCATION = (
+    '{REDIS_PORT_6379_TCP_ADDR}:{REDIS_PORT_6379_TCP_PORT}:2'
+    .format(**os.environ)
+)
 CACHES = {
     'default': {
         'BACKEND': 'redis_cache.cache.RedisCache',
-        'LOCATION': getenv('TEST_REDIS_CACHE_LOCATION', 'localhost:6379:2'),
+        'LOCATION': getenv('TEST_REDIS_CACHE_LOCATION', DEFAULT_TEST_REDIS_CACHE_LOCATION),
         'OPTIONS': {
             'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
             'PASSWORD': getenv('TEST_REDIS_CACHE_PASSWORD', None),
