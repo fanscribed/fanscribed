26db04f084f5bde1da7e559f9c0c30929033e36c
Matthew Scott <matt@11craft.com>
2015-01-01 23:15:50 -0600

    Insert rollbar script into ember apps

--- fanscribed/views.py
+++ fanscribed/views.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.http import HttpResponse, HttpResponseServerError
+from django.template import RequestContext
+from django.template.loader import get_template
 from django.views.generic import View
 
 
 class EmberIndexView(View):
+
+    # TODO: cache the rewritten index page
 
     base_url = '/'
 
@@ -17,6 +21,12 @@ class EmberIndexView(View):
         if index_file:
             with open(index_file, 'rb') as f:
                 index_content = f.read().decode('utf8')
+            # Insert Rollbar script.
+            rollbar_template = get_template('_rollbar.html')
+            rollbar_script = rollbar_template.render(RequestContext(request))
+            # Ember scripts start in body, so place rollbar at end of head.
+            index_content = index_content.replace('</head>', rollbar_script + '</head>')
+            # Rewrite references to assets so they point to /static
             if settings.DEBUG:
                 index_content = index_content.replace(
                     'href="assets/',
