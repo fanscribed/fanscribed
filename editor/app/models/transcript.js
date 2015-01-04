5c9089630bcce47814eda2515b3cea234e77817a
Matthew Scott <matt@11craft.com>
2015-01-04 01:21:52 -0600

    Link transcript editor back to transcript detail

--- editor/app/models/transcript.js
+++ editor/app/models/transcript.js
@@ -5,5 +5,16 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   state: DS.attr('string'),
   length: DS.attr('number'),
-  length_state: DS.attr('string')
+  length_state: DS.attr('string'),
+  slug: function () {
+    // http://stackoverflow.com/a/1054862/72560
+    return this.get('title')
+      .toLowerCase()
+      .replace(/[^\w ]+/g,'')
+      .replace(/ +/g,'-')
+      ;
+  }.property('title'),
+  detailUrl: function() {
+    return '/transcripts/' + this.get('id') + '-' + this.get('slug') + '/';
+  }.property('slug')
 });
