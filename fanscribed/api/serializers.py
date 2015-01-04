84deac644fa02a11d54b09f8c2d4940c5f6726ce
Matthew Scott <matt@11craft.com>
2015-01-04 16:16:14 -0600

    Include sentences w/ transcript

--- fanscribed/api/serializers.py
+++ fanscribed/api/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
 
-from ..apps.transcripts.models import Transcript
+from ..apps.transcripts.models import Sentence, Speaker, Transcript
+
+
+class SpeakerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Speaker
+        fields = ('id', 'name')
+
+
+class SentenceSerializer(serializers.ModelSerializer):
+
+    latest_speaker = SpeakerSerializer(read_only=True)
+
+    class Meta:
+        model = Sentence
+        fields = ('id', 'latest_text', 'latest_start', 'latest_speaker')
 
 
 class TranscriptSerializer(serializers.HyperlinkedModelSerializer):
 
+    sentences = SentenceSerializer(many=True, read_only=True)
+
     class Meta:
         model = Transcript
-        fields = ('url', 'id', 'title', 'state', 'length', 'length_state')
+        fields = ('url', 'id', 'title', 'state', 'length', 'length_state', 'sentences')
