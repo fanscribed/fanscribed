8cfaf59ad2a7686da2a12c557b85c30499aa85f5
Matthew Scott <matt@11craft.com>
2015-01-04 18:23:03 -0600

    Use time-code formatting for sentence start times

---
+++ editor/app/components/time-code/component.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import numeral from 'numeral';
+
+export default Ember.Component.extend({
+  hoursMinutesSeconds: function() {
+    return numeral(this.get('time')).format('00:00:00.00');
+  }.property('time'),
+  hundredths: function() {
+    var time = this.get('time');
+    var hundredths = (time - Math.floor(time));
+    return numeral(hundredths).format('.00');
+  }.property('time')
+});
