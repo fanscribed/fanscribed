e8a0a2ff2cb123fe54d6bd27c8398bdc3a6ca148
Matthew Scott <matt@11craft.com>
2015-02-16 19:06:17 -0600

    Run tests locally correctly even when DJANGO_SETTINGS_MODULE is already set

--- runtests.py
+++ runtests.py
@@ -5,7 +5,8 @@ from django.core.management import execute_from_command_line
 
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fanscribed.settings.test')
+    test_settings_module = os.environ.setdefault('DJANGO_TEST_SETTINGS_MODULE', 'fanscribed.settings.test')
+    os.environ['DJANGO_SETTINGS_MODULE'] = test_settings_module
 
     # Take arguments passed and send them to test runner.
     sys.argv.insert(1, 'test')
